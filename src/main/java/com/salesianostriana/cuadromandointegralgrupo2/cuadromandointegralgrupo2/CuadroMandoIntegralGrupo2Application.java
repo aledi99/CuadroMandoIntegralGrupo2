83a43727b97b542fc00f35acf313e677f4bc632e
Esperanza Escacena <espemacem@gmail.com>
2019-10-20 19:45:41 +0200

    arreglado encoding csv, daba problemas, falta arreglar comillas y join

--- src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/CuadroMandoIntegralGrupo2Application.java
+++ src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/CuadroMandoIntegralGrupo2Application.java
@@ -1,7 +1,12 @@
 package com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.procesador.ProcesadorCsv;
 
 @SpringBootApplication
 public class CuadroMandoIntegralGrupo2Application {
@@ -9,5 +14,11 @@ public class CuadroMandoIntegralGrupo2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(CuadroMandoIntegralGrupo2Application.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner init( ApplicationContext context, ProcesadorCsv csv) {
+		return args -> {	
+			csv.salvarValoresIndicadores("PSM_Final_csv.csv", true);
+			
+		};
+	}
 }
--- src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/procesador/ProcesadorCsv.java
+++ src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/procesador/ProcesadorCsv.java
@@ -18,14 +18,14 @@ import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.
 
 public class ProcesadorCsv {
 	private static final String DELIMITADOR =";";
-	private static final int PROCESO=1;
-	private static final int INDICADOR=2;
-	private static final int ETAPA=3;
-	private static final int CICLO=4;
-	private static final int CURSO=5;
-	private static final int UNIDAD=6;
-	private static final int VALORR=7;
-	private static final int VALORC=8;
+	private static final int PROCESO=0;
+	private static final int INDICADOR=1;
+	private static final int ETAPA=2;
+	private static final int CICLO=3;
+	private static final int CURSO=4;
+	private static final int UNIDAD=5;
+	private static final int VALORR=6;
+	private static final int VALORC=7;
 	private static final ValorIndicadorServicio valorindicadorservicio= new ValorIndicadorServicio();
 	private static final IndicadorServicio indicadorservicio = new IndicadorServicio();
 	private static final ValorIndicadorCentroServicio valorcentroservicio= new ValorIndicadorCentroServicio();
@@ -34,13 +34,11 @@ public class ProcesadorCsv {
 	public  static ValorIndicador mapToValorIndicador(String linea) {
 		String [] datos=linea.split(DELIMITADOR);
 		
-		if(datos[VALORR].equals("NA")) {
-			
-		}
+		
 		return ValorIndicador.builder()
 				.esNoAplica(datos[VALORR].equals("NA")?true:false)
-				.valorConforme(Double.valueOf(datos[VALORC]))
-				.valorReal(datos[VALORR].equals("NA")?-1.00:Double.valueOf(datos[VALORR]))
+				.valorConforme(Double.parseDouble(datos[VALORC]))
+				.valorReal(datos[VALORR].equals("NA")?-1.00:Double.parseDouble(datos[VALORR].replace(",", ".")))
 				.indicador(indicadorservicio.buscarPorNombre(datos[INDICADOR]))
 				.psm(null)
 				.build();
