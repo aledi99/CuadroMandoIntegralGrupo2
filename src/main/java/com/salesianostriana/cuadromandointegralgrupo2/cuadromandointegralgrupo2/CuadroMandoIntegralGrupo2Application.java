3431527549e1fca750797aaf08b842a13c03b922
Alvaro <alvaromarquez98@gmail.com>
2019-10-29 23:43:19 +0100

    Procesador csv estructuraCentro implementado

--- src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/CuadroMandoIntegralGrupo2Application.java
+++ src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/CuadroMandoIntegralGrupo2Application.java
@@ -9,25 +9,29 @@ import org.springframework.context.annotation.Bean;
 
 import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.files.FileStorageProperties;
 import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.procesador.ProcesadorCsv;
+import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.servicio.CursoServicio;
+import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.servicio.EtapaServicio;
 import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.servicio.IndicadorServicio;
+import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.servicio.UnidadServicio;
 import com.salesianostriana.cuadromandointegralgrupo2.cuadromandointegralgrupo2.servicio.ValorIndicadorServicio;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-    FileStorageProperties.class
-})
+@EnableConfigurationProperties({ FileStorageProperties.class })
 public class CuadroMandoIntegralGrupo2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CuadroMandoIntegralGrupo2Application.class, args);
 	}
+
 	@Bean
-	public CommandLineRunner init( ApplicationContext context, ProcesadorCsv csv,
-			IndicadorServicio indicadorservicio, ValorIndicadorServicio valorindicadorservicio) {
-		return args -> {	
-			System.out.println("busqueda "+indicadorservicio.buscarPorNombre("IND 06.2 E Mejora en indicadores de la satisfacción del cliente"));
-			csv.salvarValoresIndicadores("PSM_Final_csv.csv", true,indicadorservicio, valorindicadorservicio);
-			
+	public CommandLineRunner init(ApplicationContext context, ProcesadorCsv csv, IndicadorServicio indicadorservicio,
+			ValorIndicadorServicio valorindicadorservicio, EtapaServicio etapaServicio,
+			CursoServicio cursoServicio, UnidadServicio unidadServicio) {
+		return args -> {
+			System.out.println("busqueda " + indicadorservicio
+					.buscarPorNombre("IND 06.2 E Mejora en indicadores de la satisfacción del cliente"));
+			csv.salvarValoresIndicadores("PSM_Final_csv.csv", true, indicadorservicio, valorindicadorservicio);
+
 		};
 	}
 }
--- src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/colegio/model/Unidad.java
+++ src/main/java/com/salesianostriana/cuadromandointegralgrupo2/cuadromandointegralgrupo2/colegio/model/Unidad.java
@@ -8,12 +8,14 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 public class Unidad {
 	@Id
